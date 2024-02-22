6a24e9212615b420d6446e4a32af8bef6561efca
YurtseniukV <virayurtseniuk@gmail.com>
2024-02-22 14:23:35 +0200

    hw_1

---
+++ hw_1/main.js
@@ -0,0 +1,67 @@
+let x = 'hello';
+    console.log(x);
+
+    let z = 'owu';
+    console.log(z);
+
+    let q = 'com';
+    console.log(q);
+
+    let d = 'ua';
+    console.log(d);
+
+    let e = 1;
+    console.log(e);
+
+    let f = 10;
+    console.log(f);
+
+    let g = -999;
+    console.log(g);
+
+    let h = 123;
+    console.log(h);
+
+    let PI = 3.14;
+    console.log(PI);
+
+    let j = 2.7;
+    console.log(j);
+
+    let k = 16;
+    console.log(k);
+
+    let n = true;
+    console.log(n);
+
+    let w = false;
+    console.log(w);
+
+
+// --------------------------------------------------------------
+
+let firstName = 'Vira';
+let middleName = 'Serhiivna';
+let lastName = 'Yurtseniuk';
+let person = `${lastName}  ${firstName}  ${middleName}`;
+console.log(person);
+
+//-------------------------------------------------------------------
+
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+
+// -------------------------------------------------------------------
+
+
+
+
+
+
+
