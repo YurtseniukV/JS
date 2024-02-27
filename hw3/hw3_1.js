cc9a9f9bf87fbb3ee84c86b2a12ff66e1d46ad22
YurtseniukV <virayurtseniuk@gmail.com>
2024-02-27 15:56:47 +0200

    hw3

---
+++ hw3/hw3_1.js
@@ -0,0 +1,43 @@
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+
+for (let i = 0; i < 10; i++) {
+    document.write(` <div>Nice to meet you</div>`)
+
+}
+
+
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+let items=[
+    {greeting:'Hello'},
+    {greeting:'Hi'},
+    {greeting:'Bonjour'},
+    {greeting:'Привіт'},
+    {greeting:'Salut'},
+    {greeting:'Ola'},
+    {greeting:'Guten tag'},
+    {greeting:'Nin Hao'},
+    {greeting:'Salve'},
+    {greeting:'Asalam'}
+];
+
+for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    document.write(`<div> ${item.greeting} </div>`)
+}
+
+
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+
+let i = 0;
+while (i<20){
+    document.write(`<h1>World</h1>`)
+    i++;
+}
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+    let content = ['One', 'two', 'three', 'four', 'five', 'six', 'seven', 'Olya', 'Ira', 'Petya', 'Aa', 'Bb', 'CC', 'DD', 'SS', 'FF', 'PP', 'Monday', 'Friday', 'Sunday'];
+    let ii = 0;
+    while (ii<20){
+        let item = content[ii];
+        document.write(`<h1>${item}</h1>`);
+        ii++;
+    }
