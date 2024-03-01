bc8debe8b78c8a56ea9264ae04dd37d2c1b1ce87
YurtseniukV <virayurtseniuk@gmail.com>
2024-03-01 14:51:34 +0200

    hw4

--- hw4/hw4_1.js
+++ hw4/hw4_1.js
@@ -165,8 +165,6 @@ let qq= swap([2, 6, 9, 10],6,66);
 
 
 
-
-
 //
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
