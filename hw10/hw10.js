682e298b4c711f1c0714f83a96d1bd874eebd9ca
YurtseniukV <virayurtseniuk@gmail.com>
2024-03-16 17:36:38 +0200

    hw10

--- hw10/hw10.js
+++ hw10/hw10.js
@@ -1,38 +1,37 @@
-
 let form = document.forms['form'];
-form.onsubmit = function(e){
+form.onsubmit = function (e) {
     e.preventDefault();
 }
 let inputUsername = form.username;
 
-inputUsername.oninput = function (){
+inputUsername.oninput = function () {
+
 }
 
 let inputSurname = form.surname;
-inputSurname.oninput = function (){
+inputSurname.oninput = function () {
+
 }
 
 let inputAge = form.age;
-inputAge.oninput = function (){
+inputAge.oninput = function () {
+
 }
-
-
 let divObj = document.createElement('div');
 document.body.appendChild(divObj);
-let button = document.getElementsByClassName('createObj')[0];
-button.onclick = function (){
 
-    // let user = {
-    //     name:inputUsername.value,
-    //     age:inputAge,
-    //     surname:inputSurname
-    // };
-    //
-    // divObj.innerHTML = `<p>${user}</p>`;
+let button = document.getElementsByClassName('createOb')[0];
+button.onclick = function () {
 
-divObj.innerText = `${inputUsername.value} ${inputSurname.value}, вік : ${inputAge.value}`;
+    divObj.innerText = `${inputUsername.value} ${inputSurname.value}, вік : ${inputAge.value}`;
 
 }
 
 
-
+// let user = {
+//     name:inputUsername.value,
+//     age:inputAge,
+//     surname:inputSurname
+// };
+//
+// divObj.innerHTML = `<p>${user}</p>`;
