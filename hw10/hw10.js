b2352d6f00b45d5752c9770ede25e7316933f668
YurtseniukV <virayurtseniuk@gmail.com>
2024-03-17 00:17:45 +0200

    hw10 task 1

--- hw10/hw10.js
+++ hw10/hw10.js
@@ -1,24 +1,38 @@
+// let users = [];
+// let divObj = document.createElement('div');
+// document.body.appendChild(divObj);
+
 let form = document.forms['form'];
 form.onsubmit = function (e) {
     e.preventDefault();
+
+// let obj = {
+//     name: this.username.value,
+//     surname: this.surname.value,
+//     age: this.age.value
+// }
+// users.push(obj);
+// console.log(users);
+    // divObj.innerText = JSON.stringify(users);
+
 }
+
 let inputUsername = form.username;
-
 inputUsername.oninput = function () {
-
 }
 
 let inputSurname = form.surname;
 inputSurname.oninput = function () {
-
 }
 
 let inputAge = form.age;
 inputAge.oninput = function () {
-
 }
+
 let divObj = document.createElement('div');
 document.body.appendChild(divObj);
+
+
 
 let button = document.getElementsByClassName('createOb')[0];
 button.onclick = function () {
@@ -27,11 +41,3 @@ button.onclick = function () {
 
 }
 
-
-// let user = {
-//     name:inputUsername.value,
-//     age:inputAge,
-//     surname:inputSurname
-// };
-//
-// divObj.innerHTML = `<p>${user}</p>`;
